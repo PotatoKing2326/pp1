067e89f7cec76686d77a9dc522da13bc791f443c
Krystian Gwizdała <krgw@ailleron.com>
2022-11-18 13:30:13 +0100

    18.11

---
+++ 04-Subroutines/zad 18.py
@@ -0,0 +1,8 @@
+def number(n):
+    n = str(n)
+    suma = 0
+    for number in n:
+        suma += int(number)
+    print(suma)
+
+number(7182)
---
+++ 04-Subroutines/zad 19.py
@@ -0,0 +1,7 @@
+def f(x, y, value):
+    if value >= x and value <= y:
+        return True
+    else:
+        return False
+
+print(f(10, 20, 35))
